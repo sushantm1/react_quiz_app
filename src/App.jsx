e87c36369987924bf71b19a03bc836e9125184f7
sushantm1 <sushantm1100202@gmail.com>
2026-01-12 18:36:13 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from './Components/Quiz/quiz'
+const App = () => {
+  return (
+    <>
+    <Quiz/>
+    </>
+    
+  )
+}
+
+export default App
---
+++ src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+
+  return (
+    <div className='container'>
+      <h1>Quiz App</h1>
+    <hr />
+    <h2>1. Which of the foloeig </h2>
+    <ul>
+      {/* <li>option 1</li> */}
+      <li>option 2</li>
+      <li>option 3</li>
+      <li>option 4</li>
+    </ul>
+    <button>Next</button>
+    <div className='index'>
+      Question 1 of 10
+    </div>
+    </div>
+  )
+}
+
+export default Quiz
